11d07d9ca9230e167ec117f1ef478bafe3315ff1
pragatimahajan28 <2025d2r026@mietjammu.in>
2025-11-03 23:38:36 +0530

    Factorial of a Number

---
+++ Experiment4A.c
@@ -0,0 +1,18 @@
+// Factorial of a Number using Loop
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    long fact = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) {
+        fact = fact * i;
+    }
+
+    printf("Factorial of %d = %ld\n", n, fact);
+    return 0;
+}  
